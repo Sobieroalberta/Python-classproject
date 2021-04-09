77cce1b816e9bb814b510758213bde9870feeec6
unknown <sophie.obiero77@gmail.com>
2021-04-09 16:32:18 -0500

    added assignment 2&3

---
+++ Assignment2.py
@@ -0,0 +1,8 @@
+# Enter first and last name
+name = input('Enter your first and last name: ')
+print(name)
+print()
+#Enter your goals for this class
+print('What are your short term and long term goals for this class')
+goals = input('What would you like to be...after all said and done with this class')
+print(goals)
